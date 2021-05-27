04f5182c41d468fa93f390c32f8631a29ddcabd7
LloydKun <nosmas14@gmail.com>
2021-05-27 18:48:22 +0800

    first commit

---
+++ huarache.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nike Huarache</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="landing">
+        <div class="nav">
+            <div class="logo">
+                <svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" version="1.1" width="420.32086" height="149.8" viewBox="135.5 361.375 420.32086 149.8" overflow="visible" enable-background="new 135.5 361.375 200 72" xml:space="preserve" id="svg4" sodipodi:docname="Logo_NIKE.svg" inkscape:version="1.0.1 (3bc2e813f5, 2020-09-07)">
+                    <metadata id="metadata10">
+                        <rdf:RDF>
+                            <cc:Work rdf:about="">
+                                <dc:format>image/svg+xml</dc:format>
+                                <dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+                            </cc:Work>
+                        </rdf:RDF>
+                    </metadata>
+                    <defs id="defs8" />
+                    <sodipodi:namedview pagecolor="#ffffff" bordercolor="#666666" borderopacity="1" objecttolerance="10" gridtolerance="10" guidetolerance="10" inkscape:pageopacity="0" inkscape:pageshadow="2" inkscape:window-width="1600" inkscape:window-height="837" id="namedview6" showgrid="false" inkscape:zoom="5.21" inkscape:cx="353.93987" inkscape:cy="50.589086" inkscape:window-x="-8" inkscape:window-y="-8" inkscape:window-maximized="1" inkscape:current-layer="svg4" inkscape:document-rotation="0" />
+                    <path d="m 181.86457,511.11411 c -12.52385,-0.49755 -22.77022,-3.92442 -30.78203,-10.28929 -1.52903,-1.21591 -5.17253,-4.86157 -6.39486,-6.39916 -3.24891,-4.08526 -5.45776,-8.06114 -6.93105,-12.47237 -4.53342,-13.57891 -2.20024,-31.39748 6.6737,-50.9531 7.59794,-16.74202 19.32186,-33.34686 39.77599,-56.34437 3.01299,-3.38402 11.98555,-13.28082 12.04347,-13.28082 0.0216,0 -0.46749,0.84706 -1.08296,1.87861 -5.31833,8.90818 -9.86893,19.40114 -12.34799,28.48523 -3.9823,14.57611 -3.50195,27.08494 1.40678,36.78443 3.38615,6.68225 9.19133,12.47025 15.71914,15.66981 11.42798,5.59929 28.15931,6.0625 48.59201,1.35536 1.40679,-0.32599 71.11561,-18.83079 154.90943,-41.12278 83.79385,-22.29414 152.36394,-40.52017 152.37467,-40.50516 0.0237,0.0193 -194.67687,83.33278 -295.74887,126.555 -16.00652,6.84309 -20.28691,8.57151 -27.8119,11.21354 -19.23607,6.75513 -36.46708,9.97833 -50.39553,9.42507 z" id="path2" style="stroke-width:2.14451" />
+                </svg>
+            </div>
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+
+        <div class="nikeheader">
+            <h3>Nike Air</h3>
+            <h1>HUARACHE</h1>
+        </div>
+        <div class="shoeheader">
+            <img src="images/huarache.png" alt="">
+        </div>
+        <div class="shoedesc">
+            <p>Designed for comfort, the Nike Air Huarache brings back a street-level favourite. The iconic heel clip and stripped-away branding mix in the early '90s look you love.</p>
+        </div>
+    </div>
+    <div class="bottompart">
+        <div class="four-images">
+            <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/d1948c63-3ff3-45ff-b121-dbb28d06da08/air-huarache-shoe-kxl21N.png" alt="">
+            <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/dea833cf-90c8-4094-9aeb-6a4d4c83dc7a/air-huarache-shoe-kxl21N.png" alt="">
+            <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/7797db7e-541f-498c-a6a8-eadd69b46cd0/air-huarache-shoe-kxl21N.png" alt="">
+            <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/45031827-3149-46f1-9888-5e5bc7ebe57e/air-huarache-shoe-kxl21N.png" alt="">
+        </div>
+        <div class="description">
+            <h4>Huarache Origin</h4>
+            <p>The Nike Air Huarache released in 1991. Footwear designer Tinker Hatfield wanted to combine the fit of neoprene water-ski boots with the functionality of South American sandals.</p>
+            <p><span>Colour:</span> Bisque/Rope/White</p>
+            <p><span>Style:</span> DH9532-201</p>
+            <p>Standard delivered 5-9 Business Days
+                Express delivered 2-4 Business Days
+            </p>
+        </div>
+        <div class="addtobag">
+            <input type="button" value="Add to Bag">
+        </div>
+    </div>
+</body>
+
+</html>
